ed709d2549a02183fdd0d7c6a053c2b7687cf6db
Agastus Joyson <agastusjoyson@gmail.com>
2023-05-08 16:46:37 +0530

    Initial commit

---
+++ components/initial.js
@@ -0,0 +1,11 @@
+
+function Initial() {
+    return(
+        <div>
+            <div>AGASTUS JOYSON</div>
+            <div>FRONT END DEVELOPER</div>
+        </div>
+    );
+}
+
+export default Initial;

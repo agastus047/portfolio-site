bcebebc465b186c43f2b2a513a8828cce508aca0
Agastus Joyson <agastusjoyson@gmail.com>
2023-05-09 10:23:07 +0530

    Set up homepage background

---
+++ components/test.js
@@ -0,0 +1,7 @@
+export default function Test() {
+    return(
+        <div>
+            <div className="text-2xl mb-10">Text</div>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Initial from "@/components/initial"
+import Test from "@/components/test"
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Initial/>
+      <Test/>
     </main>
   )
 }
